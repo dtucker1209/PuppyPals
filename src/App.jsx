8554bdbd177acc388711ed06568c05ecb07de7e4
D-Money Tucker <d-moneytucker@D-Moneys-MacBook-Pro.local>
2024-11-07 21:50:42 -0600

    my second commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,27 @@ import { useState } from "react";
 function App() {
 
 const [puppies, setPuppies] = useState(puppyList);
+const [featPupId, setFeatPupId] = useState(null);
+// console.log(puppies);
+const featuredPup = puppies.find((pup)=> pup.id ===featPupId);
+console.log(featuredPup);
 
-console.log(puppies);
+
   return (
     <>
       <div className="App">
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+          </ul> 
+        </div>
+       )}
     {
       puppies.map((puppy) =>{
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
       })
     }
       </div>
